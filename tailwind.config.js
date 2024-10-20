b3cbf0301311144227bc95510f114d816a13542a
Rubén Y <rubenmeju@outlook.es>
2024-10-20 15:17:19 +0200

    fix lightning

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,30 +3,16 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      keyframes: {
-        "fly-bat": {
-          "0%": { transform: "translateX(0vw) translateY(20)" },
-          "25%": { transform: "translateX(70vw) translateY(40vh)" },
-          "40%": { transform: "translateX(110vw) translateY(40vh)" },
-          "42% ": {
-            transform: "translateX(110vw) translateY(40vh) scale(-1, 1)",
-          },
-          "50%": { transform: "translateX(50) translateY(50vh) scale(-1, 1)" },
-
-          "60%": { transform: "translateX(0) translateY(70vh) scale(-1, 1)" },
-          "70%": {
-            transform: "translateX(-20vw) translateY(50vh) scale(-1, 1)",
-          },
-          "100%": {
-            transform: "translateX(-100vw) translateY(0) scale(-1, 1)",
-          },
-        },
-      },
       animation: {
-        "fly-bat": "fly-bat 10s ease-in-out infinite",
+        flash: "flash 0.2s",
+      },
+      keyframes: {
+        flash: {
+          "0%, 100%": { opacity: 0.05 },
+          "50%": { opacity: 1 },
+        },
       },
     },
   },
-  variants: {},
   plugins: [],
 };
